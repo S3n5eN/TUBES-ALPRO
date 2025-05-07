a9ea9a91abe04aa79766b7bb6fb48d8cdfbe1ad1
S3n5eN <bbuku974@gmail.com>
2025-05-07 18:47:48 +0700

    Update TUBES.go

--- TUBES.go
+++ TUBES.go
@@ -1,11 +1,32 @@
-package main
+func isiJurnal(A *tabTidur, n *int){
+/*I.S terdapat array kosong sebagai tempat untuk menyimpan durasi tidur
+F.S array durasi tidur terisi oleh jam tidur, menit tidur, jam bangun, menit bangun selama n hari */
+	
+	var i int
+	var tSiang bool
+	//pengondisian apabila n melebihi NMAX
+	if *n > NMAX{
+		*n = NMAX
+	}
 
-import"fmt"
+	for i = 0; i < *n; i++{
+		fmt.Println("Hari ke-", i++)
+		fmt.Print("Apakah anda tidur siang?(true/false) ");fmt.Scan(&tSiang)
 
-func main(){
-	//Deklarasi variabel lokal procedure main
-	var jTidur, mTidur, jBangun, mBangun int
-	var aktivitas string
-	gdgdgdgg	
-	var kskoskm int
+		//Pengondisian apakah pengguna melakukan tidur siang atau tidak
+		
+		if tSiang == true{
+		//Tidur siang
+		fmt.Println("Silahkan isi jurnal dengan format (jam tidur siang, menit tidur siang, jam bangun siang, menit bangun siang)")
+		fmt.Scan(&A[i].jTidurSiang, &A[i].mTidurSiang, &A[i].jBangunSiang, &A[i].mBangunSiang)
+
+		//Tidur malam
+		fmt.Println("Silahkan isi jurnal dengan format (jam tidur malam, menit tidur malam, jam bangun malam, menit bangun malam)")
+		fmt.Scan(&A[i].jTidurMalam, &A[i].mTidurMalam, &A[i].jBangunMalam, &A[i].mBangunMalam)
+		}else if tSiang == false{
+			//Tidur malam
+		fmt.Println("Silahkan isi jurnal dengan format (jam tidur malam, menit tidur malam, jam bangun malam, menit bangun malam)")
+		fmt.Scan(&A[i].jTidurMalam, &A[i].mTidurMalam, &A[i].jBangunMalam, &A[i].mBangunMalam)	
+		}
+	}
 }
